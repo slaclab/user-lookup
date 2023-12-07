a4153a18e34c71d328a072f7b608f108e038ca8e
yee379 <yee379@gmail.com>
2023-12-07 09:41:06 -0800

    feat: binding to windows ldap

--- Makefile
+++ Makefile
@@ -9,6 +9,15 @@ PIP_BIN?=$(PYTHON_DIR)/pip3
 VENV_BIN?=$(PYTHON_DIR)/activate
 UVICORN?=$(PYTHON_DIR)/uvicorn
 
+LDAP_SERVER ?= ldaps://ldap-ad.slac.stanford.edu:636 
+LDAP_USER_BASEDN ?= DC=win,DC=slac,DC=Stanford,DC=edu
+LDAP_BIND_USERNAME ?= CN=osmaint,OU=Service-Accounts,OU=SCS,DC=win,DC=slac,DC=stanford,DC=edu
+SECRET_PATH ?= secret/tid/scs/osmaint
+
+secrets:
+	mkdir -p etc/.secrets
+	vault kv get --field=password -format=table $(SECRET_PATH) > etc/.secrets/ldap.password
+
 build:
 	$(CONTAINER_RT) build -t $(REPO)/$(POD):$(TAG) .
 
@@ -33,4 +42,4 @@ start:
 	source $(VENV_BIN) && $(UVICORN) main:app --host 0.0.0.0 --reload
 
 start-container:
-	$(CONTAINER_RT) run -p 8000:8000 -v ./:/app -it $(REPO)/$(POD):$(TAG) 
+	$(CONTAINER_RT) run -p 8000:8000 -v ./:/app -e SOURCE_LDAP_SERVER=$(LDAP_SERVER) -e SOURCE_LDAP_BIND_USERNAME=$(LDAP_BIND_USERNAME) -e SOURCE_LDAP_BIND_PASSWORD='$(shell cat etc/.secrets/ldap.password)' -e SOURCE_LDAP_USER_BASEDN=$(LDAP_USER_BASEDN) -it $(REPO)/$(POD):$(TAG) 
--- schema.py
+++ schema.py
@@ -14,26 +14,32 @@ from os import environ
 import logging
 
 LOG = logging.getLogger(__name__)
+logging.basicConfig( level=logging.DEBUG )
 
-LDAP_SERVER = environ.get('LDAP_SERVER', 'ldaps://sdfldap001.sdf.slac.stanford.edu' )
+SOURCE_LDAP_SERVER = environ.get('SOURCE_LDAP_SERVER', 'ldaps://sdfldap001.sdf.slac.stanford.edu' )
+SOURCE_LDAP_USER_BASEDN = environ.get('SOURCE_LDAP_USER_BASEDN',None)
+SOURCE_LDAP_BIND_USERNAME = environ.get('SOURCE_LDAP_BIND_USERNAME',None)
+SOURCE_LDAP_BIND_PASSWORD = environ.get('SOURCE_LDAP_BIND_PASSWORD',None)
 
-LDAP_DB = LDAPClient( LDAP_SERVER ).connect()
-LDAP_BASEDN = 'dc=sdf,dc=slac,dc=stanford,dc=edu'
+SOURCE_LDAP_CLIENT = LDAPClient( SOURCE_LDAP_SERVER )
+if SOURCE_LDAP_BIND_USERNAME and SOURCE_LDAP_BIND_PASSWORD:
+    SOURCE_LDAP_CLIENT.set_credentials("SIMPLE", user=SOURCE_LDAP_BIND_USERNAME, password=SOURCE_LDAP_BIND_PASSWORD)
+SOURCE_LDAP_DB = SOURCE_LDAP_CLIENT.connect()
 
+logging.debug(f"connecting to {SOURCE_LDAP_SERVER} with {SOURCE_LDAP_BIND_USERNAME} {SOURCE_LDAP_BIND_PASSWORD}, using basedn {SOURCE_LDAP_USER_BASEDN}")
 
-
-def map_to_entities_to_users( entity: List[dict] ) -> User:
-    LOG.debug(f'translate {entity}')
+def map_entities_to_users( entity: List[dict], overrides: dict={ 'fullname': 'name', 'preferredemail': 'extensionAttribute5' }  ) -> User:
+    #LOG.debug(f'translate {entity}')
     for e in entity:
         u = User(
             dn=e['dn'],
             username=e['uid'][0],
-            fullname=e['gecos'][0],
+            fullname=e[overrides['fullname'] if 'fullname' in overrides else 'gecos'][0],
             uidnumber=e['uidNumber'][0],
             shell=e['loginShell'][0],
             eppns=e['mail'],
-            preferredemail=e['mail'][0],
-            homedirectory=e['homeDirectory'][0])
+            preferredemail=e[overrides['preferredemail'] if 'preferredemail' in overrides else 'mail'][0],
+            homedirectory=e['homeDirectory'][0] if 'homeDirectory' in e else f"/sdf/home/{e['uid'][0][0]}/{e['uid'][0]}")
         yield u 
 
 def reduce_filter( filter ) -> dict:
@@ -57,5 +63,7 @@ def user_filter( filter, keys={ 'username': 'uid', 'fullname': 'gecos' } ) -> st
 class Query:
     @strawberry.field
     def users(self, info: Info, filter: UserInput ) -> List[User]:
-        e = LDAP_DB.search( LDAP_BASEDN, bonsai.LDAPSearchScope.SUB, user_filter( filter ) )
-        return map_to_entities_to_users( e )
+        logging.debug(f"querying {filter}, with {user_filter(filter)}")
+        e = SOURCE_LDAP_DB.search( SOURCE_LDAP_USER_BASEDN, bonsai.LDAPSearchScope.SUB, user_filter( filter ) )
+        #logging.debug(f"found {e}")
+        return map_entities_to_users( e )
